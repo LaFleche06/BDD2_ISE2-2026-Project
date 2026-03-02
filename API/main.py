dd08b61cf3e601b9822c698c25e3a295550e59fc
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2026-03-02 22:34:00 +0000

    feat(api): add docstring to api-test

--- API/main.py
+++ API/main.py
@@ -1,3 +1,15 @@
+"""
+API exposant un endpoint de test
+permettant de vérifier la connexion à une base de données AWS RDS.
+
+L’endpoint /test-aws :
+- Injecte une session SQLAlchemy
+- Exécute une requête SQL simple
+- Retourne la version du serveur si la connexion réussit
+- Retourne un message d’erreur en cas d’échec
+
+"""
+
 from fastapi import FastAPI, Depends
 from sqlalchemy import text
 from sqlalchemy.orm import Session
